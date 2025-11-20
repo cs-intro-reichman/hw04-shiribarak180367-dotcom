6863fe6afb47ea9834400c25ae9b3614b2840ba1
Shiri Barak <shiri.barak@post.runi.ac.il>
2025-11-20 11:16:42 +0200

    hw4

--- MyString.java
+++ MyString.java
@@ -18,13 +18,62 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        if(str == null){
+            return null;
+        }
+        String faString = "";
+	for(int i = 0 ; i < str.length(); i++){
+		char c = str.charAt(i) ;
+		if(Character.isLetter(c)){
+			 c = Character.toLowerCase(c);
+			
+        }
+        else{
+           
+        }
+        faString = faString + c;
+    }
+    return faString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
-    }
-}
+        if(str2 == null){
+            return false;
+        }
+        if(str1 == null){
+            return false;
+        }
+        if ( str1.length() < str2.length()){
+           return false; 
+        }
+        if ( str2.length()== 0){
+            return true;
+        }
+     for( int i = 0 ; i <= str1.length()- str2.length(); i++){ 
+        boolean match = true;
+       
+        for( int j = 0 ; j < str2.length(); j++){
+            if ( str1.charAt(i +j) != str2.charAt(j)){
+                match = false;
+                    break;
+
+                }
+            
+            }if (match) {
+            return true;
+            }
+        
+        }
+         return false;
+            }
+        }
+    
+          
+            
+            
+              
+            
+    
+
+
