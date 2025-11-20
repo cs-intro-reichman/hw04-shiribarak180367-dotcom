9909dcca2228898ea5c094df59bfeae09e0e4c1c
Shiri Barak <shiri.barak@post.runi.ac.il>
2025-11-20 11:34:24 +0200

    homework 4

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -29,7 +29,7 @@ public class KeywordsDetector {
         String word = keywords[j]; 
         String Newword = MyString.lowerCase(word);
         if(MyString.contains(NewSentences, Newword)== true){
-            String result = NewSentences;
+            String result = sentence;
             System.out.println(result);
             
 
