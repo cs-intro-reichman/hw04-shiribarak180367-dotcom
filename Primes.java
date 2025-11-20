509ebcbdf05a2c6db6c9923e27703c8ff5e942fc
Shiri Barak <shiri.barak@post.runi.ac.il>
2025-11-20 11:19:52 +0200

    hw 4

--- Primes.java
+++ Primes.java
@@ -36,7 +36,7 @@ public class Primes {
        
        
 
-  System.out.println(" There are " + count + " primes between 2 and " + num + " (" + f + "% are primes)");
+  System.out.println("There are " + count + " primes between 2 and " + num + " (" + f + "% are primes)");
     
      
 }
